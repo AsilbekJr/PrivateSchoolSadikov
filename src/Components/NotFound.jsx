6530091ba982ab206dfcc1623bcce58fb672feb8
AsilbekJr <89327572+AsilbekJr@users.noreply.github.com>
2023-01-23 21:14:43 +0500

    updated

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 
 const NotFound = () => {
   return (
-    <Box sx={{width:"100vw", height:"100vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
+    <Box sx={{width:"100%", height:"100vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
              <h1>This page not found</h1>
     </Box>
   )
